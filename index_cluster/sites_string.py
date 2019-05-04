e26411149c66c0aa1d68fe3729adc3c21f9ada02
Phuurl <1835431+Phuurl@users.noreply.github.com>
2019-05-04 13:51:10 +0100

    Adds very basic assert tests to `sites_string.py`

--- index_cluster/sites_string.py
+++ index_cluster/sites_string.py
@@ -3,7 +3,7 @@ import sys
 
 def icm(num_sites):
     sites_string = ""
-    for i in range(int(sys.argv[2])):
+    for i in range(num_sites):
         sites_string += "site{},".format(str(i + 1))
     # Remove trailing comma
     return sites_string[:len(sites_string) - 1]
@@ -26,8 +26,19 @@ def idx(num_indexers, num_sites, num_current_idx):
     return sites_string
 
 
+def test():
+    assert icm(1) == "site1"
+    assert icm(5) == "site1,site2,site3,site4,site5"
+    assert idx(1, 1, 1) == "site1"
+    assert idx(3, 2, 1) == "site1"
+    assert idx(3, 2, 2) == "site1"
+    assert idx(3, 2, 3) == "site2"
+
+
 if __name__ == "__main__":
     if sys.argv[1] == "icm":
         print(icm(int(sys.argv[2])))
     elif sys.argv[1] == "idx":
         print(idx(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])))
+    elif sys.argv[1] == "test":
+        test()
