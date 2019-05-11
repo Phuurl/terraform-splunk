dbd8fbf1137f7ffb8aeed12b02a19f431caf37a6
Phuurl <1835431+Phuurl@users.noreply.github.com>
2019-05-11 19:51:35 +0100

    Restructures project to be role/provider Helps make future progress on #10 easier

--- index_cluster/digitalocean/main.tf
+++ index_cluster/digitalocean/main.tf
@@ -28,7 +28,7 @@ resource "digitalocean_droplet" "icm" {
   ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
   tags = ["${digitalocean_tag.icm.id}"]
   provisioner "local-exec" {
-    command = "sleep 30; echo '${self.ipv4_address} ansible_python_interpreter=/usr/bin/python3' > icm_host; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i './icm_host' -e 'prefix=${var.prefix}' -e sites_string=$(python sites_string.py icm ${var.num_sites}) -e 'splunk_password=${var.splunk_password}' -e 'splunk_download=${var.splunk_download}' master.yml -t icm"
+    command = "sleep 30; echo '${self.ipv4_address} ansible_python_interpreter=/usr/bin/python3' > ../ansible/icm_host; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '../ansible/icm_host' -e 'prefix=${var.prefix}' -e sites_string=$(python ../ansible/sites_string.py icm ${var.num_sites}) -e 'splunk_password=${var.splunk_password}' -e 'splunk_download=${var.splunk_download}' ../ansible/master.yml -t icm"
   }
 }
 
@@ -42,7 +42,7 @@ resource "digitalocean_droplet" "idx" {
   ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
   tags = ["${digitalocean_tag.idx.id}"]
   provisioner "local-exec" {
-    command = "sleep 30; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' -e 'ansible_python_interpreter=/usr/bin/python3' -e 'prefix=${var.prefix}' -e sites_string=$(python sites_string.py idx ${var.num_indexers} ${var.num_sites} ${count.index + 1}) -e 'splunk_password=${var.splunk_password}' -e 'idx_num=${count.index + 1}' -e 'icm_ip=${digitalocean_droplet.icm.ipv4_address_private}' -e 'splunk_download=${var.splunk_download}' master.yml -t idx"
+    command = "sleep 30; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' -e 'ansible_python_interpreter=/usr/bin/python3' -e 'prefix=${var.prefix}' -e sites_string=$(python ../ansible/sites_string.py idx ${var.num_indexers} ${var.num_sites} ${count.index + 1}) -e 'splunk_password=${var.splunk_password}' -e 'idx_num=${count.index + 1}' -e 'icm_ip=${digitalocean_droplet.icm.ipv4_address_private}' -e 'splunk_download=${var.splunk_download}' ../ansible/master.yml -t idx"
   }
 }
 
