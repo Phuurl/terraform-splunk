c55bb365924deca6790f6bcd91840a46f1885255
Phuurl <1835431+Phuurl@users.noreply.github.com>
2019-05-11 19:24:13 +0100

    Adds ability to deploy apps from files/icm/master-apps

--- index_cluster/main.tf
+++ index_cluster/main.tf
@@ -28,7 +28,7 @@ resource "digitalocean_droplet" "icm" {
   ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
   tags = ["${digitalocean_tag.icm.id}"]
   provisioner "local-exec" {
-    command = "sleep 30; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' -e 'ansible_python_interpreter=/usr/bin/python3' -e 'prefix=${var.prefix}' -e sites_string=$(python sites_string.py icm ${var.num_sites}) -e 'splunk_password=${var.splunk_password}' -e 'splunk_download=${var.splunk_download}' master.yml -t icm"
+    command = "sleep 30; echo '${self.ipv4_address} ansible_python_interpreter=/usr/bin/python3' > icm_host; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i './icm_host' -e 'prefix=${var.prefix}' -e sites_string=$(python sites_string.py icm ${var.num_sites}) -e 'splunk_password=${var.splunk_password}' -e 'splunk_download=${var.splunk_download}' master.yml -t icm"
   }
 }
 
